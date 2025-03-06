007610f67052c808107e05085766302a0f49f61e
Daniel Vargas <dvargasp@javeriana.edu.co>
2025-03-05 22:11:31 -0500

    Rename BackApplication to MainApplication

---
+++ src/main/java/com/bridgecare/back/MainApplication.java
@@ -0,0 +1,22 @@
+package com.bridgecare.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class MainApplication {
+
+	static {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
