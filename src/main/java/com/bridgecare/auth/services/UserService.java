9e350848ec50747a8f42e0163fdcf7ea4ab6f628
Daniel Vargas <dvargasp@javeriana.edu.co>
2025-03-19 23:18:29 -0500

    Moved DTO to common library

--- src/main/java/com/bridgecare/auth/services/UserService.java
+++ src/main/java/com/bridgecare/auth/services/UserService.java
@@ -2,8 +2,8 @@ package com.bridgecare.auth.services;
 
 import org.springframework.stereotype.Service;
 
-import com.bridgecare.auth.models.dtos.UsuarioDTO;
 import com.bridgecare.common.models.entities.Usuario;
+import com.bridgecare.common.models.dtos.UsuarioDTO;
 import com.bridgecare.auth.repositories.UserRepository;
 import com.bridgecare.auth.security.services.JWTService;
 
